724fa824d7bf6dbaef98c92ec2336d6008310c5a
Bash Elliott <bashelliott@gmail.com>
2025-11-20 19:28:11 +1100

    add react-native-paper and nativewind (react native tailwind)

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Appbar, Text } from "react-native-paper";
+
+import "./global.css"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<Appbar.Header>
+				<Appbar.Content title="Elevate Horizon Connect" />
+			</Appbar.Header>
+			<View className="flex-1 items-center justify-center bg-black">
+				<Text>This is where stuff goes! :)</Text>
+				<StatusBar style="auto" />
+			</View>
+		</>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import { registerRootComponent } from 'expo';
+import App from "./App";
+import { PaperProvider } from "react-native-paper";
+import { AppRegistry } from "react-native";
 
-import App from './App';
+export default function Main() {
+	return (
+		<PaperProvider>
+			<App />
+		</PaperProvider>
+	);
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent("main", () => Main);
